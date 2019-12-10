23af1e93f23895edfdfe679b7f6011bf1edc6bdf
Robert Bovee <Rjbovee112@gmail.com>
2019-12-09 21:02:25 -0700

    questions added

--- Assets/questions.js
+++ Assets/questions.js
@@ -21,10 +21,48 @@ function selectAnswer() {
 
 const questions = [
     {
-        question: 'What is the brithday of America?',
+        question: 'When did history start? (Hint: Before this date, everything else was a mistake.),
         answers: [
             { text: '1776', correct: true },
-            { text: '1876', correct: false }
+            { text: '457', correct: false },
+            { text: '0', correct: false },
+            { text: '1492', correct: false }
+        ]
+    }
+]
+
+const questions = [
+    {
+        question: 'How long did World War 2 last?',
+        answers: [
+            { text: '5 years 1 day', correct: true },
+            { text: '3 years 354 days', correct: false },
+            { text: '4 years 67 days', correct: false },
+            { text: '7 years 15 days ', correct: false }
+        ]
+    }
+]
+
+const questions = [
+    {
+        question: 'What is the only country to land humans on the moon?,
+        answers: [
+            { text: 'USA', correct: true },
+            { text: 'China', correct: false },
+            { text: 'USSR', correct: false },
+            { text: 'United Kingdom', correct: false }
+        ]
+    }
+]
+
+const questions = [
+    {
+        question: 'On what day was the state of Utah founded?',
+        answers: [
+            { text: 'January 4, 1896', correct: true },
+            { text: 'October 15th, 1889', correct: false },
+            { text: 'September 5th, 1901 ', correct: false },
+            { text: 'May, 18th 1876', correct: false }
         ]
     }
 ]
