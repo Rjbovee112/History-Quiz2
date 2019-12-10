ec2c1f6cfa9581ef1364f2ed51d9f3f864fe2a8f
Robert Bovee <Rjbovee112@gmail.com>
2019-12-09 22:07:06 -0700

    delted qeustions, trying to figure out first question

--- Assets/questions.js
+++ Assets/questions.js
@@ -1,8 +1,11 @@
 const startButton = document.getElementById('start-btn')
 const questionContainerEl = document.getElementById('question-container')
+const questionEl = document.getElementById('question')
+const answerButtonEl = document.getElementById('answer-buttons')
+
 startButton.addEventListener('click', startGame)
 
-const shuffledQuestions, currentQuestionIndex
+let shuffledQuestions, currentQuestionIndex
 
 function startGame() {
     console.log('Started')
@@ -14,8 +17,14 @@ function startGame() {
 }
 
 function nextQuestion() {
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+    questionEl.innerText = question.question
 
 }
+
 
 function selectAnswer() {
 
@@ -23,7 +32,7 @@ function selectAnswer() {
 
 const questions = [
     {
-        question: ('When did history start? (Hint: Before this date, everything else was a mistake.),
+        question: 'When did history start?',
         answers: [
             { text: '1776', correct: true },
             { text: '457', correct: false },
@@ -33,38 +42,3 @@ const questions = [
     }
 ]
 
-const questions = [
-    {
-        question: ('How long did World War 2 last?'),
-        answers: [
-            { text: '5 years 1 day', correct: true },
-            { text: '3 years 354 days', correct: false },
-            { text: '4 years 67 days', correct: false },
-            { text: '7 years 15 days ', correct: false }
-        ]
-    }
-]
-
-const questions = [
-    {
-        question: ('What is the only country to land humans on the moon?),
-        answers: [
-            { text: 'USA', correct: true },
-            { text: 'China', correct: false },
-            { text: 'USSR', correct: false },
-            { text: 'United Kingdom', correct: false }
-        ]
-    }
-]
-
-const questions = [
-    {
-        question: ('On what day was the state of Utah founded?'),
-        answers: [
-            { text: 'January 4, 1896', correct: true },
-            { text: 'October 15th, 1889', correct: false },
-            { text: 'September 5th, 1901 ', correct: false },
-            { text: 'May, 18th 1876', correct: false }
-        ]
-    }
-]
