26640388d77a741d3fbc6b82269b3a6fc701bba0
Robert Bovee <Rjbovee112@gmail.com>
2019-12-09 20:41:50 -0700

    maybe adding questions?/click functions

--- Assets/questions.js
+++ Assets/questions.js
@@ -1,12 +1,14 @@
 const startButton = document.getElementById('start-btn')
-
+const questionContainerEl = document.getElementById('question-container')
 startButton.addEventListener('click', startGame)
 
 
 
 function startGame() {
     console.log('Started')
-
+    startButton.classList.add('hide')
+    questionContainerEl.classList.remove('hide')
+    nextQuestion()
 }
 
 function nextQuestion() {
@@ -16,3 +18,13 @@ function nextQuestion() {
 function selectAnswer() {
 
 }
+
+const questions = [
+    {
+        question: 'What is the brithday of America?',
+        answers: [
+            { text: '1776', correct: true },
+            { text: '1876', correct: false }
+        ]
+    }
+]
